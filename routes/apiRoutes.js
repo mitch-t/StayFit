f9a56e392ab0ba1374dc4283ae08c1b632f89488
Tom Mitchell <mitcht032@gmail.com>
2020-12-06 20:40:11 -0800

    fixed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,48 @@
-var db = require("../models");
+const Workout = require("../models/exercise.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-    // Creates a new workout in the workout database
-    app.post("/api/workouts", async (req, res)=> {
-        try{
-            const response = await db.Workout.create({type: "workout"})
-            res.json(response);
-        }
-        catch(err){
-            console.log("error occurred creating a workout: ", err)
-        }
-    });
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .limit(7)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-    // Used by api.js to get last workout
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({})
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-    });
-}
-   
+  app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id, {
+      $push: {
+        exercises: body,
+      },
+    })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,12 @@
-
-// the path package to get the correct file path for our html
-
 var path = require("path");
 
-module.exports = function(app) {
-  // Called when "Countinue Workout" or "new Workout" is clicked in index.html
-  app.get("/exercise", function(req, res) {
+module.exports = function (app) {
+  
+  app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-  // Called when "Dashboard" is clicked in index.html
-  app.get("/stats", function(req, res) {
+
+  app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 };
